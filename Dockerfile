960e76c8893a83f8e8ea73a82090590a58c8704b
ytan29392 <ytan29392@gmail.com>
2025-11-23 18:05:23 +0300

    Add docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use official Node image
+FROM node:20
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,10 +1,20 @@
+// import axios from "axios";
+
+// const axiosClient = axios.create({
+//   baseURL: "http://localhost:8000", // FastAPI URL
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default axiosClient;
+
+
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000", // FastAPI URL
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  headers: { "Content-Type": "application/json" }
 });
 
 export default axiosClient;
